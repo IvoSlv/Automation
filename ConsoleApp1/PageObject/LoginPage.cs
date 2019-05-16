79446b658d7fa2df4efe4738bd5b8fbcae5b31ed
islavchev.cw@mmm.com <islavchev@DEV-18>
2019-05-16 17:38:37 +0300

    Initial commit

---
+++ ConsoleApp1/PageObject/LoginPage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using ConsoleApp1.Extensions;
+using ConsoleApp1.TestDataAccess;
+
+namespace ConsoleApp1.PageObject
+{
+    class LoginPage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='Email']")]
+        [CacheLookup]
+        public IWebElement Email { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='Password']")]
+        [CacheLookup]
+        public IWebElement Password { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//button[@class='bk-button bk-button-block bk-margin-bottom-1']")]
+        [CacheLookup]
+        public IWebElement Submit { get; set; }
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void LoginToApplication(string testName)
+        {
+            var userData = ExcelDataAccess.GetTestData(testName);
+            Email.SendKeys(userData.Username);
+            Password.SendKeys(userData.Password);
+            Submit.Submit();
+            //Email.EnterText(userData.Username, "drake@abv.bg");
+            //Password.Clear();
+            //Password.SendKeys("Dsa_123");
+        }
+
+    }
+}
---
+++ ConsoleApp1/TestCases/LogInTest.cs
@@ -0,0 +1,111 @@
+﻿using ConsoleApp1.PageObject;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace ConsoleApp1
+{
+    
+    class LogInTest
+    {
+        
+
+        [Test, Order(1)]
+        public void KraftBoardMenuTest()
+        {
+
+            IWebDriver driver = new ChromeDriver(@"D:\_Development\ChromeDriverOld");
+            driver.Url = "https://www.bindkraft.com/en/";
+          
+            var homePage = new HomePage(driver);
+            homePage.WindowMaximize();
+            homePage.KraftBoardMenu();
+            driver.Close();
+        }
+
+        [Test, Order(2)]
+        public void KraftBoardPlansMenuTest()
+        {
+
+            IWebDriver driver = new ChromeDriver(@"D:\_Development\ChromeDriverOld");
+            driver.Url = "https://www.bindkraft.com/en/";
+
+            var homePage = new HomePage(driver);
+            homePage.WindowMaximize();
+            homePage.KraftBoardPlansMenu();
+            driver.Close();
+        }
+
+        [Test, Order(3)]
+        public void KraftCrmMenuTest()
+        {
+
+            IWebDriver driver = new ChromeDriver(@"D:\_Development\ChromeDriverOld");
+            driver.Url = "https://www.bindkraft.com/en/";
+
+            var homePage = new HomePage(driver);
+            homePage.WindowMaximize();
+            homePage.KraftCrmMenu();
+            driver.Close();
+        }
+
+        [Test, Order(4)]
+        public void KraftCrmPlansMenuTest()
+        {
+
+            IWebDriver driver = new ChromeDriver(@"D:\_Development\ChromeDriverOld");
+            driver.Url = "https://www.bindkraft.com/en/";
+
+            var homePage = new HomePage(driver);
+            homePage.WindowMaximize();
+            homePage.KraftCrmPlansMenu();
+            driver.Close();
+        }
+
+        [Test, Order(5)]
+        public void FeaturesMenuTest()
+        {
+
+            IWebDriver driver = new ChromeDriver(@"D:\_Development\ChromeDriverOld");
+            driver.Url = "https://www.bindkraft.com/en/";
+
+            var homePage = new HomePage(driver);
+            homePage.WindowMaximize();
+            homePage.FeaturesMenu();
+            driver.Close();
+        }
+
+        [Test, Order(6)]
+        public void LoginTest()
+        {
+
+            IWebDriver driver = new ChromeDriver(@"D:\_Development\ChromeDriverOld");
+            driver.Url = ConfigurationManager.AppSettings["URL"];
+            //driver.Url = "https://www.bindkraft.com/en/";
+
+            var homePage = new HomePage(driver);
+            homePage.WindowMaximize();
+            homePage.GoToLoginPage();
+
+            var loginPage = new LoginPage(driver);
+            loginPage.LoginToApplication("LogInTest");
+
+            driver.Close();
+        }
+
+
+        
+    }
+}
