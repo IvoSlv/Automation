7169e69cf1bca13fc5cbc9eda27130d5260f041e
Konstantin Antonov <kantonov@cleancodefactory.de>
2019-05-22 18:35:13 +0300

    Added ExtentReports library and configured it. It outputs test report in ~rootSolution/TestResults as HTML.

--- BindKraftAutomation/Models/PageTestBase.cs
+++ BindKraftAutomation/Models/PageTestBase.cs
@@ -1,4 +1,6 @@
-﻿using BindKraftAutomation.Globals;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using BindKraftAutomation.Globals;
 using BindKraftAutomation.PageObject;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -14,12 +16,16 @@ namespace BindKraftAutomation.Models
     {
         internal IWebDriver driver;
         internal LandingPage homePage;
+        internal ExtentHtmlReporter htmlReport = new ExtentHtmlReporter(@"C:\Users\kantonov\Source\repos\Automation\TestResults\Report.html");
+        internal ExtentReports extent = new ExtentReports();
 
         internal void InitDriver(string url = Constants.BINDKRAFT_URL)
         {
             this.driver = new ChromeDriver(Constants.chromeDriverPath);
             this.driver.Url = url;
             this.homePage = new LandingPage(this.driver);
+            htmlReport.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            extent.AttachReporter(htmlReport);
         }
 
     }
--- BindKraftAutomation/TestCases/LandingPageTests.cs
+++ BindKraftAutomation/TestCases/LandingPageTests.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium.Chrome;
 using BindKraftAutomation.Globals;
 using System;
 using BindKraftAutomation.Models;
+using AventStack.ExtentReports.Gherkin.Model;
 
 namespace BindKraftAutomation
 {
@@ -18,6 +19,15 @@ namespace BindKraftAutomation
             InitDriver();
             this.homePage.GoToLoginPage();
             this.driver.Dispose();
+            
+            //The test reporting feature is based in the PageTestBase class.
+            //Every test need to have those three steps - Feature -> Scenario -> (not sure the create node is needed, this needs sume further testing)
+            var feature = extent.CreateTest<Feature>("Go to Login");
+            var scenario = feature.CreateNode<Scenario>("Navigation to login");
+            scenario.CreateNode<Given>("I naviggate to login page");
+
+            //Here further testing is needed as well. This might be needed only on the last test. Practically this creates the file/appends the result tests.
+            extent.Flush();
         }
 
         [Test, Order(2)]
