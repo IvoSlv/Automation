0367e3ec06b9fe46069e6f54bebae732a5ac8b92
Konstantin Antonov <kantonov@cleancodefactory.de>
2019-05-23 17:42:52 +0300

    Fixed rooth solution path for html reporter.

--- BindKraftAutomation/Models/PageTestBase.cs
+++ BindKraftAutomation/Models/PageTestBase.cs
@@ -6,11 +6,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace BindKraftAutomation.Models
 {
@@ -18,7 +17,7 @@ namespace BindKraftAutomation.Models
     {
         internal IWebDriver driver;
         internal LandingPage homePage;
-        internal ExtentHtmlReporter htmlReport = new ExtentHtmlReporter(@"C:\Users\islavchev\source\repos\Automate\TestResults\Report.html");
+        internal ExtentHtmlReporter htmlReport;
         internal ExtentReports extent = new ExtentReports();
 
         internal void InitDriver(string url = Constants.BINDKRAFT_URL)
@@ -26,6 +25,10 @@ namespace BindKraftAutomation.Models
             this.driver = new ChromeDriver(Constants.chromeDriverPath);
             this.driver.Url = url;
             this.homePage = new LandingPage(this.driver);
+            var rootPath = new DirectoryInfo(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                .Parent.Parent.Parent;
+            this.htmlReport = new ExtentHtmlReporter(rootPath.FullName + "\\TestResults\\Report.html");
             htmlReport.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
             extent.AttachReporter(htmlReport);
         }
